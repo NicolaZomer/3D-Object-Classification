8ad6add51acbd325664b6b13081e2ea9988acf15
nicolezattarin <72666452+nicolezattarin@users.noreply.github.com>
2023-02-07 12:09:33 +0100

    minor changes

--- train.py
+++ train.py
@@ -29,7 +29,11 @@ import os
 
 parser = argparse.ArgumentParser(description='training')
 parser.add_argument('--model_name', type=str, default='pointnet', help='model name (default: pointnet)', choices=['pointnet', 'voxnet', 'res_voxnet'])
+<<<<<<< Updated upstream
 parser.add_argument('--epochs', type=int, default=100, help='number of epochs to train (default: 10)')
+=======
+parser.add_argument('--epochs', type=int, default=40, help='number of epochs to train (default: 10)')
+>>>>>>> Stashed changes
 parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 0.001)')
 parser.add_argument('--batch_size', type=int, default=64, help='batch size (default: 32)')
 parser.add_argument('--save_dir', type=str, default='checkpoints', help='directory to save checkpoints (default: checkpoints/pointnet)')
@@ -177,7 +181,7 @@ def main (
         # ax.set_ylabel('Label')
         # ax.set_title('Probability distribution')
         
-        plt.show()
+        # plt.show()
 
 
 
