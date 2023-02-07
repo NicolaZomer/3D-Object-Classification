Fixing triplet model and adding rotation of .txt files

TripletLoss/tripletnet.py
@@ -13,12 +13,62 @@ import torch.nn.functional as F
 
 from custom_losses import TripletCenterLoss
 
+class LinClassifier(nn.Module):
+    def __init__ (self, nfeatures, nclasses):
+        super(LinClassifier, self).__init__()
+        self.num_classes  = nclasses
+        self.num_features = nfeatures
+        
+        self.fc3 = self.fully_connected(self.num_features, self.num_classes, activation_fn=None, bn=False, name='fc3')
+        
+    def fully_connected(self,
+            input_size,
+            output_size,
+            activation_fn=nn.ReLU(),
+            bn=False,
+            name='fully_connected'):
+        """ Fully connected layer with non-linear operation.
 
-class TripletNet (nn.Module):
+        """
+
+        # create sequential model
+        model = nn.Sequential()
+
+        # add fully connected layer
+        model.add_module(name+'fc', nn.Linear(input_size, output_size))
+        if activation_fn is not None: model.add_module('activation_fn', activation_fn)
+
+        return model
     
-    def __init__ (self, nclasses, num_point=10000):
+    def forward(self, x):
+        """ Classification TripletNet, input is BxNfeatures=Bx256, output Bx40
+    
+        """
+        verbose = False
+
+        num_features = x.shape[1] # number of features
+        batch_size = x.shape[0] # batch size
+
+        num_in_channels = 1 # number of input channels
+
+        if verbose:
+            print ('num_features: ', num_features)
+            print ('batch_size: ', batch_size)
+            print ('num_in_channels: ', num_in_channels)
+
+        # unsqueeze to add channel dimension
+        #x = torch.unsqueeze(x, dim=1)# batch_size x 1 x num_features the second dimension is the channel dimension
+
+        x = self.fc3(x)
+
+        return x        
+
+
+class TripletNet(nn.Module):
+    
+    def __init__ (self, nfeatures=256, num_point=10000):
         super(TripletNet, self).__init__()
-        self.num_classes = nclasses
+        self.num_features = nfeatures
         
         self.loss = TripletCenterLoss()
 
@@ -32,14 +82,13 @@ class TripletNet (nn.Module):
         self.fc1 = self.fully_connected(1024, 512, bn=False, name='fc1')
         
         self.dropout = nn.Dropout(p=0.6)
-        self.fc2 = self.fully_connected(512, 256, bn=False, name='fc2')
+        self.fc2 = self.fully_connected(512, self.num_features, bn=False, activation_fn=None, name='fc2')
+        
         
                 
         #self.fc3 = self.fully_connected(256, self.num_classes, activation_fn=None, bn=False, name='fc3')
+        # self.custlayer = self.norm_l2_layer(self.fc3)
         
-        #self.custlayer = self.norm_l2_layer(self.fc3)
-        
-
     def conv2D(self, 
             num_in_channels,
             num_output_channels,
@@ -81,13 +130,11 @@ class TripletNet (nn.Module):
         if activation_fn is not None: model.add_module('activation_fn', activation_fn)
 
         return model
-
     
     
     def get_loss(self, input, output):
 
         return self.loss(input, output)
-
 
     def forward(self, x):
         """ Classification TripletNet, input is BxNx3, output Bx40 
@@ -133,6 +180,24 @@ class TripletNet (nn.Module):
         x = F.normalize(x, p=2, dim=1)
 
         return x
+
+
+class WholeNet(nn.Module):
+    def __init__(self, modelA, modelB):
+        '''
+        modelA: features extractor
+        modelB: linear classifier
+        '''
+        super(WholeNet, self).__init__()
+        self.modelA = modelA
+        self.modelB = modelB
+        
+    def forward(self, x):
+        x = self.modelA(x)
+        x = nn.ReLU()(x) # relu
+        x = self.modelB(x)
+        return x
+
 
 if __name__ == '__main__':
 

classifier.py
@@ -129,6 +129,7 @@ class Classifier ():
             
             
     def train_triplet(self,
+            classifier_model, 
             train_dataloader, 
             val_dataloader, 
             loss_fn, 
@@ -144,6 +145,9 @@ class Classifier ():
         self.val_loss_log = []
         
         lambda_tcl = 0.001
+                
+        self.Net_cls = classifier_model
+        self.Net_cls.to(self.device)
 
         # if save_dir doews not exist, create it
         if not os.path.exists(save_dir):
@@ -176,19 +180,28 @@ class Classifier ():
                 # print (x_batch.shape)
                 # print (label_batch.shape)
 
-                out = self.Net(x_batch)
+                out_feat = self.Net(x_batch)
 
-                # Compute loss
+                # Compute tcl loss
                 # labels are int values, so we need to convert them to long
                 if label_batch.dtype in [torch.int64, torch.int32]:
                     label_batch = label_batch.long()
-                    
+                
+                tripl_loss = self.loss_fn[1](out_feat, label_batch)
+                tcl_loss.append(tripl_loss.detach().cpu().numpy())
+                
+                # Classifier and compute cross entropy loss
+                
+                
+                out = nn.ReLU()(out_feat)
+                
+                out = self.Net_cls(out)
+                
+                
                 sf_loss = self.loss_fn[0](out, label_batch)
                 soft_loss.append(sf_loss.detach().cpu().numpy())
                 
-                tripl_loss = self.loss_fn[1](out, label_batch)
-                tcl_loss.append(tripl_loss.detach().cpu().numpy())
-
+                # Final loss
                 loss = sf_loss + (lambda_tcl*tripl_loss) 
 
                 # Backpropagation
@@ -223,14 +236,17 @@ class Classifier ():
                     label_batch = sample_batched[1].to(self.device)
 
                     # Forward pass
-                    out = self.Net(x_batch)
+                    out_feat = self.Net(x_batch)
+                    
+                    out = nn.ReLu()(out_feat)
+                    out = self.Net_cls(out)
 
                     # Compute loss cross entropy
                     
                     if label_batch.dtype in [torch.int64, torch.int32]:
                         label_batch = label_batch.long()
                         
-                    sf_loss = self.loss_fn[0](out, label_batch)
+                    sf_loss = self.loss_fn[0](out_feat, label_batch)
                     val_soft_loss.append(sf_loss.detach().cpu().numpy())
                 
                     tripl_loss = self.loss_fn[1](out, label_batch)
@@ -255,6 +271,15 @@ class Classifier ():
             # save model
             self.save_state_dict(f'{save_dir}/model_{epoch_num}.torch')
             self.save_optimizer_state(f'{save_dir}/optimizer_{epoch_num}.torch')
+            
+            # save lin classifier
+            path = f'{save_dir}/model_cls_{epoch_num}.torch'
+            if path.split('.')[-1] != 'torch':
+                path = path + '.torch'
+                
+            print (f"Saving model to {path}")
+            net_cls_state_dict = self.Net_cls.state_dict()
+            torch.save(net_cls_state_dict, path)
 
             
             np.save(f'{save_dir}/train_loss.npy', self.train_loss_log)

dataset/PointCloudDataset.py
@@ -11,13 +11,24 @@ import glob
 import pandas as pd
 import open3d as o3d
 
-def rotate_point_cloud(xyz):
+def rotate_mesh(xyz):
     angle = np.random.choice( np.arange(-np.pi/2, np.pi/2 +  np.pi/4, np.pi/4) )       
     R = xyz.get_rotation_matrix_from_xyz((0, 0, angle))
 
     xyz = xyz.rotate(R, center=(0,0,0))
     return xyz
 
+
+def Rz(theta):
+  return np.matrix([[ np.cos(theta), -np.sin(theta), 0 ],
+                   [ np.sin(theta), np.cos(theta) , 0 ],
+                   [ 0           , 0            , 1 ]])
+  
+def rotate_txt(xyz):
+    angle = np.random.choice( np.arange(-np.pi/2, np.pi/2 +  np.pi/4, np.pi/4) )
+    R = Rz(angle)
+    xyz = xyz.dot(R)
+    return xyz
 
 class PointCloudDataset(Dataset):
     def __init__(self, 
@@ -120,9 +131,11 @@ class PointCloudDataset(Dataset):
 
         # parsing is specific to the file extension
         if self.file_extension == '.off':
+            print(path)
             x = o3d.io.read_triangle_mesh(path)
             if self.rotation:
-                x = rotate_point_cloud(x)
+                
+                x = rotate_mesh(x)
                 
             x = np.asarray(x.vertices)
             # rehsape to num_pointsx3
@@ -134,7 +147,10 @@ class PointCloudDataset(Dataset):
             x = x[:, :3]
             # rehsape to num_pointsx3
             x = np.reshape(x, (-1, 3))
-
+            
+            if self.rotation:
+                x = rotate_txt(x)
+            
         # get npoints shuffle
         idx = np.arange(x.shape[0])
         np.random.shuffle(idx)

train_triplet.py
@@ -21,7 +21,7 @@ from classifier import Classifier
 #from networks.voxnet import VoxNet
 #from networks.res_voxnet import ResVoxNet
 
-from TripletLoss.tripletnet import TripletNet
+from TripletLoss.tripletnet import TripletNet, LinClassifier, WholeNet
 from TripletLoss.custom_losses import TripletCenterLoss
 
 
@@ -91,8 +91,10 @@ def main (
         dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=4)
         dataloader_val = DataLoader(dataset_val, batch_size=32, shuffle=True)
 
-        Net = TripletNet(nclasses=40)
-        parameters = Net.parameters()
+        Net_tcl = TripletNet()
+        Net_cls = LinClassifier(nfeatures=256, nclasses=40)
+                
+        parameters = Net_tcl.parameters()
         optimizer =  optim.SGD(parameters, lr=5e-4, weight_decay=1e-5)
 
 
@@ -105,28 +107,40 @@ def main (
     if len(models_saved) > 0:
         # get most recent model
         epoches_done = max([int(model.split('_')[-1].split('.')[0]) for model in models_saved])
-        model_path = os.path.join(save_dir, f'model_{epoches_done}.torch')
-        print(f"Loading model from {model_path}")
-        Net.load_state_dict(torch.load(model_path))
+        model_tcl_path = os.path.join(save_dir, f'model_{epoches_done}.torch')
+        print(f"Loading model from {model_tcl_path}")
+        Net_tcl.load_state_dict(torch.load(model_tcl_path))
+        
+        model_cls_path = os.path.join(save_dir, f'model_cls_{epoches_done}.torch')
+        print(f"Loading model from {model_cls_path}")
+        Net_cls.load_state_dict(torch.load(model_cls_path))
+        
     else:
         epoches_done = 0
 
+    # move models to device
+    Net_tcl.to(device)
+    Net_cls.to(device)
+    features_extractor = Classifier(Net_tcl, device=device)
 
-    # move model to device
-    Net.to(device)
-    classifier = Classifier(Net, device=device)
-    
+        
+
     #LOSS
     loss_cls = nn.CrossEntropyLoss()
     loss_tcl = TripletCenterLoss(margin=5)
 
     if train:
         print ("Starting training")
-        classifier.train_triplet(dataloader_train, dataloader_val, epochs=epochs, optimizer=optimizer, loss_fn = [loss_cls, loss_tcl],
-                         save_dir=save_dir, start_epoch=epoches_done+1)
+        features_extractor.train_triplet(Net_cls, dataloader_train, dataloader_val, epochs=epochs, optimizer=optimizer,
+                                         loss_fn = [loss_cls, loss_tcl], save_dir=save_dir, start_epoch=epoches_done+1)
         print ("Training done")
     else:
         print ("Starting testing")
+        # whole net
+        Net_whole = WholeNet(Net_tcl, Net_cls)
+        Net_whole.to(device)    
+        classifier = Classifier(Net_whole, device=device)
+        
         results = classifier.test(dataloader_val)
         print ("Testing done")
         print(results)